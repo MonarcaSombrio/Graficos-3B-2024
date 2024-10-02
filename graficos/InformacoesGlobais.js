9c17d45c3e780c079f066be40052a28e0a334c58
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-10-02 11:09:30 -0300

    2162

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -8,16 +8,21 @@ async function visualizarInformacoesGlobais(params) {
     //contante que armazena uma repossta  await=espera //fetch=faz a requisição
     const res = await fetch(url);
     const dados = await res.json();//espera as reposta erem convertidas em JSON
-console.log(dados);
 
-const paragrafo = document.createElement ('P');//criar um elemento de parágrafo
-paragrafo.classList.add('graficos-container_texto');//cria uma classe do CSS
-paragrafo.innerHTML = `você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </pan> de pessoas 
-e que aproximadamente <span> ${dados.total_pessoas_conectadas} </span> estão conectadas em alguma rede ocial e passam em média ${dados.tempo_medio}
-horas conectada.`
-//criara variáel"container" seleciona o ID na section do HTML
-const container = document.getElementById('graficos-container');
-container.appendChild(parágrafo);//insere o parágrafodentro do "container" 
+    const pessoasConectadoas = (dados.total_pessoas_conectadas / 1e9);// cria uma variável, pega os "dados.total_pessoas_conectadas" e divide por 1 bilhão
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9);
+    const horas = parseInt(dados.tempo_medio / 1e9);
+    const minutos = Marh.round((dados.tempo_medio-horas) * 60);//cria uma variável que utiliza apenasos minutos das horas efas o arredeendamento vom a função "Math.round"
+    console.log(dados);
+
+    const paragrafo = document.createElement('P');//criar um elemento de parágrafo
+    paragrafo.classList.add('graficos-container_texto');//cria uma classe do CSS
+    paragrafo.innerHTML = `você sabia que o mundo tem <span> ${pessoasNoMundo} bilhoes </pan> de pessoas 
+e que aproximadamente <span> ${pessoasConectadoas} bilhoes </span> estão conectadas em alguma rede social e passam em média <span> ${horas} horas </span>
+e ${minutos} minutos conectadas.`
+    //criara variáel"container" seleciona o ID na section do HTML
+    const container = document.getElementById('graficos-container');
+    container.appendChild(parágrafo);//insere o parágrafodentro do "container" 
 
 }
 visualizarInformacoesGlobais();
